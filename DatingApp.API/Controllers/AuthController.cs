66a9538c131ba73457d9e236756534f647de879c
rakeshjha064 <jha.rakeshjha64@gmail.com>
2020-11-26 17:50:39 +0530

    creates register method and dtos

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -1,11 +1,37 @@
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DatingApp.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthController
+    public class AuthController:ControllerBase
     {
-        
+        private readonly IAuthRepository _repo;
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+            userForRegisterDto.Username=userForRegisterDto.Username.ToLower();
+            if(await _repo.UserExists(userForRegisterDto.Username))
+              return BadRequest("Username already exists");
+              var UserToCreate=new User
+              {
+                  Username=userForRegisterDto.Username
+              };
+
+              await _repo.Register(UserToCreate,userForRegisterDto.Password);
+              return StatusCode(201);
+
+        }
+
     }
 }
